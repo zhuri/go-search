87d60099f636a4797b32a6793605d2d29958c718
zhuri <ezhuri@gmail.com>
2019-04-06 01:00:13 +0200

    Made small sql-like search

--- domain/entities/catalog.go
+++ domain/entities/catalog.go
@@ -18,6 +18,7 @@ var (
 	InvalidNameErr = errors.New("Name is not valid")
 	InvalidQtyErr  = errors.New("Invalid price")
 	DataInsertErr  = errors.New("Product can not be inserted")
+	DataGetErr     = errors.New("Product can not be get")
 )
 
 func NewCatalog() *Catalog {
@@ -50,21 +51,18 @@ func (c *Catalog) AddProduct(name, description string, qty int) error {
 func (c *Catalog) UpdateProduct(name, description string, qty int) error {
 
 	err := productDataConstraints(name, description, qty)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
+	//todo
 }
 
-func (c *Catalog) GetAllProducts() {
+func (c *Catalog) GetAllProducts() ([]Product, error) {
 	conn := data.NewConnection()
 	db := conn.Connect()
 
 	res, err := db.Query("Select * from product;")
 
 	if err != nil {
-		fmt.Println("Error happened", err)
+		return nil, DataGetErr
 	}
 
 	product := Product{}
@@ -74,7 +72,31 @@ func (c *Catalog) GetAllProducts() {
 			log.Fatalln(err)
 		}
 		fmt.Printf("%#v\n", product)
+		c.products = append(c.products, product)
 	}
+	return c.products, nil
+}
+
+func (c *Catalog) SearchProducts(name, description string) ([]Product, error) {
+	conn := data.NewConnection()
+	db := conn.Connect()
+	query := "Select * from product where name like '%" + name + "%' or description like '%" + description + "%';"
+	res, err := db.Query(query)
+
+	if err != nil {
+		return nil, DataGetErr
+	}
+
+	product := Product{}
+	for res.Next() {
+		err := res.Scan(&product.ID, &product.Name, &product.Description, &product.Qty)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("%#v\n", product)
+		c.products = append(c.products, product)
+	}
+	return c.products, nil
 }
 
 func productDataConstraints(name, description string, qty int) error {
--- main.go
+++ main.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/micro-search/go-search/data"
 	"github.com/micro-search/go-search/domain/entities"
 )
 
 func main() {
-	conn := data.NewConnection()
-	fmt.Println(conn)
-	db := conn.Connect()
+	// conn := data.NewConnection()
+	// fmt.Println(conn)
+	// db := conn.Connect()
 
-	res, err := db.Query("Select * from product;")
+	// res, err := db.Query("Select * from product;")
 
-	if err != nil {
-		fmt.Println("Error happened", err)
-	}
+	// if err != nil {
+	// 	fmt.Println("Error happened", err)
+	// }
 
-	// fmt.Println("Result", res)
+	// // fmt.Println("Result", res)
 
-	product := entities.Product{}
-	for res.Next() {
-		err := res.Scan(&product.ID, &product.Name, &product.Description, &product.Qty)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("%#v\n", product)
-	}
+	// product := entities.Product{}
+	// for res.Next() {
+	// 	err := res.Scan(&product.ID, &product.Name, &product.Description, &product.Qty)
+	// 	if err != nil {
+	// 		log.Fatalln(err)
+	// 	}
+	// 	fmt.Printf("%#v\n", product)
+	// }
+
+	catalog := entities.NewCatalog()
+	res, _ := catalog.SearchProducts("name1", "desc1")
+	fmt.Println(res)
 }
